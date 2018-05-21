9cc75f7e24cfcbee0fb3a4a954513c3e04a6be0d
Patrick McLaughlin <patrickmc21@gmail.com>
2018-05-21 10:02:54 -0600

    Add knex to server file

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
 const app = express();
+const db = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
+
+app.use(bodyParser.json());
 
 app.listen(app.get('port'), () => {
   console.log(`Server is listening on ${app.get('port')}...`)
 })
+
+module.exports = { app, db };
