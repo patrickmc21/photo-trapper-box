5ffabaa6f036fedbd16cfd5276af9bac889ed0b8
Patrick McLaughlin <patrickmc21@gmail.com>
2018-05-21 10:05:02 -0600

    initial server test setup

---
+++ tests/routes.spec.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const { app, db } = require('../server');
+
+chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+
+});
+
+describe('API routes', () => {
+
+});
